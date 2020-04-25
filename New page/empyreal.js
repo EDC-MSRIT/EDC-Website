b28855e17b8d46c5c8840998979d4264d0151265
Divya <divyasanjeevni@gmail.com>
2020-04-25 22:48:57 +0530

    new pages

---
+++ New page/empyreal.js
@@ -0,0 +1,10 @@
+let slideFromRight = {
+    origin: 'right',
+    distance: '100%',
+    opacity: '0',
+    duration: '1000',
+    easing: 'ease-in'
+}
+
+ScrollReveal().reveal('.slide-enter', slideFromRight);
+
