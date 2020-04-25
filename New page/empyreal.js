a365e21ac8281c0e3b531bfc2bbbbda818f78040
Divya <divyasanjeevni@gmail.com>
2020-04-26 00:17:29 +0530

    animations refactored

--- New page/empyreal.js
+++ New page/empyreal.js
@@ -1,9 +1,9 @@
 let slideFromRight = {
     origin: 'right',
-    distance: '100%',
-    opacity: '0',
-    duration: '1000',
-    easing: 'ease-in'
+    distance: '50%',
+    opacity: 0,
+    duration: 1000,
+    easing: 'ease'
 }
 
 ScrollReveal().reveal('.slide-enter', slideFromRight);
