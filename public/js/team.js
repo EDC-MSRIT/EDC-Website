ffab268370a2a2ca05237d7dbb4fc3078f455a41
Kushal Garg <kushalgarg2000@gmail.com>
2020-05-14 18:55:06 +0530

    Fixes team page navbar

--- public/js/team.js
+++ public/js/team.js
@@ -1,3 +1,5 @@
+console.log("Linked");
+
 // Prealoder---------------------------------------------------------------------------------------------------------------------------------
 function prealoader() {
     if ($("#preloader").length) {
@@ -16,28 +18,3 @@ function prealoader() {
   });
 
   // Prealoder Ends---------------------------------------------------------------------------------------------------------------------------------
-var slideFromLeft = {
-    delay:200,
-    distance: '50%',
-    origin: 'left',
-    opacity: 0
-};
-
-var slideFromRight = {
-    delay:200,
-    distance: '50%',
-    origin: 'right',
-    opacity: 0
-};
-
-
-var slideFromBottom = {
-    delay:200,
-    distance: '50%',
-    origin: 'bottom',
-    opacity: 0
-};
-
-ScrollReveal().reveal('.team__left',slideFromLeft)
-ScrollReveal().reveal('.team__right',slideFromRight)
-ScrollReveal().reveal('.team__middle',slideFromBottom)
